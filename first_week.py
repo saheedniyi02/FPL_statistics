d12bf7fb47fb5d4bc0f7bb6e6df67f31f152ad6c
saheedniyi02 <64325621+saheedniyi02@users.noreply.github.com>
2022-07-23 16:42:03 +0100

    Add files via upload

---
+++ first_week.py
@@ -0,0 +1,78 @@
+import requests
+
+
+def get_player_fixture_info(id, gameweek):
+    player_info = requests.get(
+        f"https://fantasy.premierleague.com/api/element-summary/{id}/"
+    ).json()
+    fixture = player_info["fixtures"][gameweek - 1]
+    home_team = get_team_name(fixture["team_h"])
+    away_team = get_team_name(fixture["team_a"])
+    kickoff_time = fixture["kickoff_time"]
+    is_home = fixture["is_home"]
+    return home_team, away_team, kickoff_time, is_home
+
+
+def get_team_name(id):
+    general_information = requests.get(
+        "https://fantasy.premierleague.com/api/bootstrap-static/"
+    ).json()
+    teams = general_information["teams"]
+    for team in teams:
+        if team["id"] == id:
+            return team["name"]
+
+
+def get_player_position(element_type):
+    general_information = requests.get(
+        "https://fantasy.premierleague.com/api/bootstrap-static/"
+    ).json()
+    positions = general_information["element_types"]
+    for position in positions:
+        if position["id"] == element_type:
+            return position["singular_name_short"]
+
+
+home_teams = []
+away_teams = []
+kickoff_times = []
+is_homes = []
+positions = []
+costs = []
+names = []
+ids = []
+general_info = requests.get(
+    f"https://fantasy.premierleague.com/api/bootstrap-static/"
+).json()
+player_infos = general_info["elements"]
+gameweek = 1
+for player_info in player_infos:
+    id = player_info["id"]
+    name = player_info["first_name"] + " " + player_info["second_name"]
+    team_id = player_info["team"]
+    cost = player_info["now_cost"]
+    element_type = player_info["element_type"]
+    position = get_player_position(element_type)
+    print(id)
+    home_team, away_team, kickoff_time, is_home = get_player_fixture_info(id, gameweek)
+    ids.append(id)
+    names.append(name)
+    costs.append(cost)
+    positions.append(position)
+    home_teams.append(home_team)
+    away_teams.append(away_team)
+    kickoff_times.append(kickoff_time)
+    is_homes.append(is_home)
+
+import pandas as pd
+
+df = pd.DataFrame()
+df["id"] = ids
+df["name"] = names
+df["cost"] = costs
+df["position"] = positions
+df["home_team"] = home_teams
+df["away_team"] = away_teams
+df["kickoff_time"] = kickoff_times
+df["is_home"] = is_homes
+df.to_csv("player_info.csv")
