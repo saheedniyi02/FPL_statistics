fcaa3f54d023f8f1f7ab023c3e0cf355d46beea0
saheedniyi02 <64325621+saheedniyi02@users.noreply.github.com>
2022-07-24 14:20:11 +0100

    Add files via upload

--- get_21_22_stats.py
+++ get_21_22_stats.py
@@ -14,12 +14,14 @@ for player_info in player_infos:
         print(id)
         stats_summary = requests.get(link).json()
         stats_2022 = stats_summary["history_past"][-1]
-        stats_22["name"]=name
+        stats_2022["name"] = name
         all_stats.append(stats_2022)
     except:
         pass
 
 import pandas as pd
 
+print(all_stats)
 df = pd.DataFrame(all_stats)
+print(df)
 df.to_csv("cleaned_players_21_22_2.csv")
